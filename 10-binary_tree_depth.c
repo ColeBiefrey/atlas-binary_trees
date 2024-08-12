406efe174caf40d89ea6a265fadb36bc67a5950d
ColeBiefrey <Cole.Kelsey@atlasschool.com>
2024-08-12 11:16:51 -0500

    10-bin

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * recursive_depth - measures the depth of node in binary tree
+ * @tree: tree root
+ * Return: depth of node in binary tree
+ */
+
+size_t recursive_depth(const binary_tree_tv*tree)
+{
+	if (tree == NULL)
+		return (-1);
+
+	return (recursive_depth(tree->parent) + 1);
+}
+
+/**
+ * binary_tree_depth - calls recursive_depth to return depth
+ * of node in binary tree
+ * @tree: tree root
+ * Return: depth of tree or 0 if tree is NULL
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (recursive_depth(tree));
+}
